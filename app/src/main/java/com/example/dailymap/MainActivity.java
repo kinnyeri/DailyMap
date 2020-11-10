f8e1f77dc4a8fe595b47bfade982b6fd9dc2f151
Kim Yeri <98sena@naver.com>
2020-11-10 10:11:08 +0900

    init with Diary

---
+++ app/src/main/java/com/example/dailymap/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.dailymap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View v){
+        Intent intent = new Intent(this,Diary.class);
+        startActivity(intent);
+    }
+}
